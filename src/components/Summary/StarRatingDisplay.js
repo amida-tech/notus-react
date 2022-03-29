a6a477463369b4570028145117cab0884812a8a3
Tim <tim@Amidas-MacBook-Pro.local>
2022-03-29 15:39:32 -0400

    quick commit

--- src/components/Summary/StarRatingDisplay.js
+++ src/components/Summary/StarRatingDisplay.js
@@ -7,53 +7,57 @@ import { Grid, Typography, Rating } from '@mui/material';
 function StarRatingDisplay({ activeMeasure }) {
   const starsTip = 'Star rating subject to change depending on measures and other resources. For more information, please contact NCQA.';
 
-  const ratingPresentUI = (
-    <>
-      <Grid className="rating-trends__header-align">
-        <Typography variant="h3" className="rating-trends__h3-header">
-          Star Rating
+  function ratingPresentUI() {
+    return (
+      <>
+        <Grid className="rating-trends__header-align">
+          <Typography variant="h3" className="rating-trends__h3-header">
+            Star Rating
+          </Typography>
+          <ToolTip title={starsTip}>
+            <HelpIcon className="rating-trends__help-icon" fontSize="small" />
+          </ToolTip>
+        </Grid>
+        <Rating
+          className="rating-trends__star-rating"
+          name="read-only"
+          value={activeMeasure.starRating || 0}
+          precision={0.5}
+          readOnly
+        />
+        <Typography className="rating-trends__star-rating-label">
+          {activeMeasure.label && `(${activeMeasure.label})`}
         </Typography>
-        <ToolTip title={starsTip}>
-          <HelpIcon className="rating-trends__help-icon" fontSize="small" />
-        </ToolTip>
-      </Grid>
-      <Rating
-        className="rating-trends__star-rating"
-        name="read-only"
-        value={activeMeasure.starRating || 0}
-        precision={0.5}
-        readOnly
-      />
-      <Typography className="rating-trends__star-rating-label">
-        {activeMeasure.starRating < 0 ? '(Not Applicable)' : activeMeasure.label && `(${activeMeasure.label})`}
-      </Typography>
 
-    </>
+      </>
 
-  )
-  const ratingNotPresentUI = (
-    <>
-      <Grid className="rating-trends__header-align">
-        <Typography variant="h3" className="rating-trends__h3-header">
-          Star Rating
+    )
+  }
+  function ratingNotPresentUI() {
+    return (
+      <>
+        <Grid className="rating-trends__header-align">
+          <Typography variant="h3" className="rating-trends__h3-header">
+            Star Rating
+          </Typography>
+          <ToolTip title={starsTip}>
+            <HelpIcon className="rating-trends__help-icon" fontSize="small" />
+          </ToolTip>
+        </Grid>
+        <Typography className="rating-trends__not-applicable">
+          N/A
         </Typography>
-        <ToolTip title={starsTip}>
-          <HelpIcon className="rating-trends__help-icon" fontSize="small" />
-        </ToolTip>
-      </Grid>
-      <Typography className="trend-display__percent-change">
-        N/A
-      </Typography>
-      <Typography className="rating-trends__star-rating-label">
-        {activeMeasure.starRating < 0 ? '(Not Applicable)' : activeMeasure.label && `(${activeMeasure.label})`}
-      </Typography>
-    </>
-  )
+        <Typography>
+          (Not Applicable)
+        </Typography>
+      </>
+    )
+  }
 
   if (activeMeasure.starRating > 0) {
-    return ratingPresentUI
+    return ratingPresentUI()
   }
-  return ratingNotPresentUI
+  return ratingNotPresentUI()
 }
 
 StarRatingDisplay.propTypes = {
@@ -66,8 +70,8 @@ StarRatingDisplay.propTypes = {
 
 StarRatingDisplay.defaultProps = {
   activeMeasure: {
-    // starRating: 0,
-    // label: '',
+    starRating: 0,
+    label: '',
   },
 }
 export default StarRatingDisplay
