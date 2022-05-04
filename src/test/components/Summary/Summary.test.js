0fd23f88eb93bc3c691ac95f597227023fefaf89
Eric <eawest2@gmail.com>
2022-05-04 16:18:56 -0400

    I should commit my changes more often.

--- src/test/components/Summary/Summary.test.js
+++ src/test/components/Summary/Summary.test.js
@@ -2,9 +2,13 @@ import React from 'react';
 import {
   fireEvent, render, screen,
 } from '@testing-library/react';
+import { sortedTrendsCreator, mainTrendCreator } from 'components/Summary/RatingTrendsUtils';
 import Info from '../../../components/Summary/Info';
 import TrendDisplay from '../../../components/Summary/TrendDisplay';
+import RatingTrends from '../../../components/Summary/RatingTrends';
 import Banner from '../../../components/Summary/Banner'
+import { DatastoreReducer } from '../../../context/DatastoreReducer';
+import { resultList, infoObject, trendList } from '../../data/DemoData';
 
 describe('Banner', () => {
   test('checks that the timestamp rendered', () => {
@@ -53,5 +57,55 @@ describe('TrendDisplay', () => {
     expect(screen.queryByText('EFG Score % Change')).not.toBeNull()
     expect(screen.queryByText('-50 %')).not.toBeNull()
     expect(container.firstChild.classList.contains('trend-display--width-50')).toBe(true)
+  })
+})
+describe('RatingTrends', () => {
+  const mockInitState = {
+    results: [],
+    trends: [],
+    currentResults: [],
+    info: {},
+    lastUpdated: 'Updating now...',
+  }
+  const mockDataStore = DatastoreReducer(mockInitState, { type: 'SET_RESULTS', payload: { results: resultList, info: infoObject } })
+  const mockActiveMeasure = {
+    _id: 'composite-2022-02-27',
+    measure: 'composite',
+    date: '2022-02-27T00:00:00.000Z',
+    value: 45.863095238095234,
+    starRating: 2.5,
+    numerator: 78,
+    denominator: 177,
+    initialPopulation: 183,
+    exclusions: 129,
+    label: 'Composite - Composite Score',
+    shortLabel: 'Composite',
+    title: 'Composite Score',
+  }
+  const mockTrends = trendList;
+
+  test('tests mainTrendCreator', () => {
+    const MockMeasureTrend = mockTrends
+      .find((trend) => trend.measure === mockActiveMeasure.measure);
+    const mainTrend = mainTrendCreator(mockActiveMeasure, mockDataStore.info, MockMeasureTrend);
+    expect(mainTrend.measure === 'Composite').toBe(true);
+    expect(mainTrend.percentChange === -9).toBe(true);
+  })
+
+  test('tests sortedTrendsCreator', () => {
+    const MockMeasureTrend = mockTrends
+      .find((trend) => trend.measure === mockActiveMeasure.measure);
+    expect(sortedTrendsCreator(mockActiveMeasure, mockTrends, MockMeasureTrend)
+      .length === 4).toBe(true);
+  })
+
+  test('tests Composite functionality', () => {
+    const { container } = render(
+      <RatingTrends
+        activeMeasure={mockActiveMeasure}
+        info={mockDataStore.info}
+        trends={mockTrends}
+      />,
+    )
   })
 })
