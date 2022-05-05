76c4a2643c7272018b86007536f2276d29c7eeb1
Tim <tim@Amidas-MacBook-Pro.local>
2022-05-05 15:43:07 -0400

    finished test

--- src/test/components/MeasureResults/MeasureResultsTable.test.js
+++ src/test/components/MeasureResults/MeasureResultsTable.test.js
@@ -17,8 +17,12 @@ const selectedMeasures = [
   'imae',
 ];
 
+let x = true;
+const mockHandleMeasureChange = jest.fn(() => !x);
 
-const mockHandleMeasureChange = jest.fn(() => false);
+const handleByLineMeasureChange = (event) => {
+  x = event.target.checked;
+};
 
 describe('MeasureResultsTable', () => {
   it('renders to screen with default conditions', () => {
@@ -40,34 +44,64 @@ describe('MeasureResultsTable', () => {
   })
 
   it('when you click any checkbox but "all," both that checkbox and "all" are unchecked', () => {
-    const { container } = render(<MeasureResultsTable
+    const { container, rerender } = render(<MeasureResultsTable
       currentResults={currentResults}
       handleMeasureChange={mockHandleMeasureChange}
       selectedMeasures={selectedMeasures}
       colorMapping={colorMapping}
     />)
     // Steps:
-    const targetInputTag = container.getElementsByTagName('input')[0]
-    // console.log(targetInputTag)
-    // screen.debug()
-    // 2) fireEvent to click on a checkbox.
-    // 3) Mock `handleMeasureChange` and confirm it was clicked.
-    // 4) Rerender MeasureResultsTable.
-    // 5) Then confirm that "all" and the selected checkboxes are both unchecked.
+    const targetInputTag = container.getElementsByTagName('input')
+    for (let i = 0; i < targetInputTag.length; i += 1) {
+      expect(targetInputTag[i].checked).toBe(true)
+    }
   })
 
   it('when you click "all," every checkbox will then be checked', () => {
-    const { container } = render(<MeasureResultsTable
+    const { container, rerender } = render(<MeasureResultsTable
       currentResults={currentResults}
-      handleMeasureChange={mockHandleMeasureChange}
+      handleMeasureChange={handleByLineMeasureChange}
       selectedMeasures={selectedMeasures}
       colorMapping={colorMapping}
     />)
-    // Steps:
-    // 1) You must change the above props so not all checkboxes will be checked when rendered.
-    // 2) fireEvent to click on "all".
-    // 3) Mock `handleMeasureChange` and confirm it was clicked.
-    // 4) Rerender MeasureResultsTable.
-    // 5) Then confirm that all checkboxes are checked.
+    const targetSVGTagAllSelectedMeasuresChecked = container.querySelectorAll('[data-testid="CheckBoxIcon"]')
+    const targetSVGTagAllSelectedMeasuresUnchecked = container.querySelectorAll('[data-testid="CheckBoxOutlineBlankIcon"]')
+    expect(targetSVGTagAllSelectedMeasuresChecked.length).toEqual(6)
+    expect(targetSVGTagAllSelectedMeasuresUnchecked.length).toEqual(0)
+
+    rerender(<MeasureResultsTable
+      currentResults={currentResults}
+      handleMeasureChange={handleByLineMeasureChange}
+      selectedMeasures={[]}
+      colorMapping={colorMapping}
+    />)
+    const targetSVGTagNoSelectedMeasuresChecked = container.querySelectorAll('[data-testid="CheckBoxIcon"]')
+    const targetSVGTagNoSelectedMeasuresUnchecked = container.querySelectorAll('[data-testid="CheckBoxOutlineBlankIcon"]')
+    expect(targetSVGTagNoSelectedMeasuresChecked.length).toEqual(0)
+    expect(targetSVGTagNoSelectedMeasuresUnchecked.length).toEqual(6)
+
+    rerender(<MeasureResultsTable
+      currentResults={currentResults}
+      handleMeasureChange={handleByLineMeasureChange}
+      selectedMeasures={['composite']}
+      colorMapping={colorMapping}
+    />)
+
+    const targetSVGTagONESelectedMeasuresChecked = container.querySelectorAll('[data-testid="CheckBoxIcon"]')
+    const targetSVGTagONESelectedMeasuresUnchecked = container.querySelectorAll('[data-testid="CheckBoxOutlineBlankIcon"]')
+    expect(targetSVGTagONESelectedMeasuresChecked.length).toEqual(1)
+    expect(targetSVGTagONESelectedMeasuresUnchecked.length).toEqual(5)
+
+    rerender(<MeasureResultsTable
+      currentResults={currentResults}
+      handleMeasureChange={handleByLineMeasureChange}
+      selectedMeasures={['composite', 'aab']}
+      colorMapping={colorMapping}
+    />)
+
+    const targetSVGTagTWOSelectedMeasuresChecked = container.querySelectorAll('[data-testid="CheckBoxIcon"]')
+    const targetSVGTagTWOSelectedMeasuresUnchecked = container.querySelectorAll('[data-testid="CheckBoxOutlineBlankIcon"]')
+    expect(targetSVGTagTWOSelectedMeasuresChecked.length).toEqual(2)
+    expect(targetSVGTagTWOSelectedMeasuresUnchecked.length).toEqual(4)
   })
 })
