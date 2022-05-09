8e0806d25d1b68ef1df980dd12128fc420fab40e
Eric West <35747608+eawest2@users.noreply.github.com>
2022-05-09 10:41:39 -0400

    Feature/sar 494 rating trend tests (#82)
    Refactored ratings and trends for testing, added front end tests for ratings and trends

---
+++ src/components/Summary/RatingTrendsUtils.js
@@ -0,0 +1,19 @@
+export function sortedTrendsCreator(activeMeasure, trends, measureTrend) {
+  let sortedTrends = [];
+  if (activeMeasure.measure === 'composite') {
+    sortedTrends = trends
+      .filter((trend) => trend.measure !== 'composite')
+      .sort((a, b) => b.percentChange - a.percentChange);
+  } else if (activeMeasure.measure) {
+    sortedTrends = measureTrend.subScoreTrends
+      .sort((a, b) => b.percentChange - a.percentChange);
+  }
+  return sortedTrends;
+}
+
+export function mainTrendCreator(activeMeasure, info, measureTrend) {
+  const mainTrend = { measure: '', percentChange: undefined };
+  mainTrend.measure = info[activeMeasure.measure] !== undefined ? info[activeMeasure.measure].displayLabel : '';
+  mainTrend.percentChange = measureTrend === undefined ? undefined : measureTrend.percentChange;
+  return mainTrend;
+}
