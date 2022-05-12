636f320b401a12df96ae71687ae377584e73532d
Keith <keithjayjones@gmail.com>
2022-05-12 12:01:24 -0400

    Provides data retrieval and simple data rendering

---
+++ src/layouts/MemberInfo.js
@@ -0,0 +1,81 @@
+import React, {
+  useEffect,
+  useState,
+} from 'react';
+import PropTypes from 'prop-types';
+import {
+  Typography, Box, List, ListItem,
+} from '@mui/material';
+
+const axios = require('axios').default;
+
+const memberQueryUrl = new URL(`${process.env.REACT_APP_HEDIS_MEASURE_API_URL}members/info/`);
+
+function MemberInfo({ id }) {
+  const [memberInfo, setMemberInfo] = useState({});
+  const values = [];
+  let measure = '';
+
+  useEffect(() => {
+    setMemberInfo({});
+    axios.get(`${memberQueryUrl}?memberId=${id}`)
+      .then((res) => {
+        setMemberInfo(res.data);
+      });
+  }, [id]);
+
+  if (memberInfo.memberId) {
+    measure = memberInfo.measurementType;
+    const memberData = memberInfo[memberInfo.memberId]
+    const denominatorFields = Object.keys(memberData).filter((field) => field.startsWith('Denominator'));
+
+    denominatorFields.forEach((denomField) => {
+      let numerField = 'Numerator';
+      if (denomField.includes(' ')) {
+        numerField += denomField.split(' ')[1];
+      }
+      let numerValue = '';
+      let denomValue = '';
+      if (Array.isArray(memberData[denomField])) {
+        denomValue = memberData[denomField].length;
+        numerValue = memberData[numerField].length;
+      } else {
+        denomValue = memberData[denomField] ? 1 : 0;
+        numerValue = memberData[numerField] ? 1 : 0;
+      }
+      values.push(
+        <ListItem key={denomField}>
+          {denomField}
+          {': '}
+          {denomValue}
+          {' - '}
+          {numerField}
+          {': '}
+          {numerValue}
+        </ListItem>,
+      )
+    });
+  }
+
+  return (
+    <Box>
+      <Typography variant="h2" className="rating-trends__h2-header">
+        {`Member Info ${id}`}
+      </Typography>
+      <Typography>
+        {`Measure: ${measure}`}
+      </Typography>
+      <List>{values}</List>
+    </Box>
+  )
+}
+
+MemberInfo.propTypes = {
+  id: PropTypes.string,
+}
+
+MemberInfo.defaultProps = {
+  id: '',
+}
+
+export default MemberInfo;
