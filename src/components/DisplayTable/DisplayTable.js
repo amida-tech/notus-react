449face4a64bf495f4212e39c4e22d38aa1263ff
Tim <tim@Amidas-MacBook-Pro.local>
2022-05-31 10:05:12 -0400

    pagination complete

--- src/components/DisplayTable/DisplayTable.js
+++ src/components/DisplayTable/DisplayTable.js
@@ -1,9 +1,10 @@
 import {
-  Divider, Grid, Pagination, PaginationItem,
+  Divider, Grid,
 } from '@mui/material';
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import StyledEngineProvider from '@mui/material/StyledEngineProvider';
+import TablePagination from '@mui/material/TablePagination';
 import { colorMappingProps } from '../ChartContainer/D3Props';
 import usePagination from '../Utilites/PaginationUtil';
 import TableHeader from './TableHeader';
@@ -18,7 +19,8 @@ function DisplayTable({
   selectedRows,
   colorMapping,
 }) {
-  const [currentPage, setCurrentPage] = useState(1);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
 
   let pageCount = 0;
   if (pageSize) {
@@ -26,10 +28,35 @@ function DisplayTable({
   }
   const pageData = usePagination(rowData, pageSize);
 
-  const handleChange = (_e, p) => {
-    setCurrentPage(p);
-    pageData.jump(p);
+  const handleChangePage = (_e, p) => {
+    setPage(p);
   };
+  const handleChangeRowsPerPage = (e) => {
+    setRowsPerPage(parseInt(e.target.value, 10));
+    setPage(0);
+  };
+  const rowsPerPageOptions = () => {
+    const MeasureLength = pageData.currentData().length
+    const optionRegular = [1, 2, 3, MeasureLength]
+    const optionXL = [1, 3, 5, 10, 15, 20, 25, MeasureLength]
+    const optionL = [1, 3, 5, 10, 15, 20, MeasureLength]
+    const optionM = [1, 3, 5, 10, 15, MeasureLength]
+    const optionS = [1, 3, 5, 10, MeasureLength]
+    const optionXS = [1, 3, 5, MeasureLength]
+
+    if (MeasureLength >= 45) {
+      return optionXL
+    } if (MeasureLength < 44 && MeasureLength > 30) {
+      return optionL
+    } if (MeasureLength < 29 && MeasureLength > 19) {
+      return optionM
+    } if (MeasureLength < 18 && MeasureLength > 12) {
+      return optionS
+    } if (MeasureLength < 11 && MeasureLength > 6) {
+      return optionXS
+    }
+    return optionRegular
+  }
 
   return (
     <Grid container className="display-table">
@@ -41,41 +68,35 @@ function DisplayTable({
         selectedRows={selectedRows}
       />
       <Divider className="display-table__header-divider" />
-      {pageData.currentData().map((item) => (
-        <Grid
-          item
-          className="display-table__row"
-          key={`chart-container-grid-measure-${item.value}`}
-        >
-          <TableRow
-            rowDataItem={item}
-            headerInfo={headerInfo}
-            useCheckBox={useCheckBox}
-            handleCheckBoxEvent={handleCheckBoxEvent}
-            rowSelected={selectedRows.includes(item.value)}
-            color={colorMapping.find((mapping) => mapping.value === item.value).color}
-          />
+      {pageData.currentData().slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+        .map((item) => (
+          <Grid
+            item
+            className="display-table__row"
+            key={`chart-container-grid-measure-${item.value}`}
+          >
+            <TableRow
+              rowDataItem={item}
+              headerInfo={headerInfo}
+              useCheckBox={useCheckBox}
+              handleCheckBoxEvent={handleCheckBoxEvent}
+              rowSelected={selectedRows.includes(item.value)}
+              color={colorMapping.find((mapping) => mapping.value === item.value).color}
+            />
+          </Grid>
 
-        </Grid>
-
-      ))}
+        ))}
       {pageCount > 0 && (
       <StyledEngineProvider injectFirst>
-        <Pagination
-          count={pageCount}
-          size="large"
-          page={currentPage}
-          variant="outlined"
-          shape="rounded"
-          onChange={handleChange}
-          classes={{ root: '.MuiPagination-root' }}
-          renderItem={(item) => (
-            <PaginationItem
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              {...item}
-              classes={{ root: '.MuiPaginationItem-root' }}
-            />
-          )}
+        <TablePagination
+          component="div"
+          rowsPerPageOptions={rowsPerPageOptions()}
+          count={pageData.currentData().length}
+          page={page}
+          className="display-table__pagination"
+          onPageChange={handleChangePage}
+          rowsPerPage={rowsPerPage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
         />
       </StyledEngineProvider>
       )}
