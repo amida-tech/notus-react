d020fe914e486a3eb601c0a36c7e01fd764743db
Keith <keithjayjones@gmail.com>
2022-06-03 23:53:11 -0400

    Formats data to display the true/false values

--- src/components/Utilites/PatientTable.js
+++ src/components/Utilites/PatientTable.js
@@ -3,35 +3,59 @@ const pageSize = 10;
 
 // These will change based on the measurement.
 const headerData = (selectedMeasures, measure, storeInfo) => {
+  let headerFlexBasis = 22;
+  if (selectedMeasures.length > 4) {
+    headerFlexBasis = selectedMeasures.length >= 6 ? 10 : 15;
+  }
+  let standardFlexBasis = 15;
+  if (selectedMeasures.length > 4) {
+    standardFlexBasis = selectedMeasures.length >= 6 ? 5 : 10;
+  }
+
   const headerInfo = [
     {
       key: 'label',
       link: true,
       header: 'MemberID',
       tooltip: memberIdTip,
-      flexBasis: selectedMeasures.length > 4 ? selectedMeasures.length >= 6 ? 10 : 15 : 22,
+      flexBasis: headerFlexBasis,
     },
   ];
   selectedMeasures.forEach((measureName) => {
     const labelFound = storeInfo[measureName].displayLabel
     headerInfo.push({
       key: measureName,
-      link: true,
+      link: false,
       header: labelFound,
-      // tooltip: memberIdTip,
-      flexBasis: selectedMeasures.length > 4 ? selectedMeasures.length >= 6 ? 5 : 10 : 15,
+      tooltip: storeInfo[measureName].title,
+      flexBasis: standardFlexBasis,
     })
-  })
+  });
   return headerInfo
 };
 
+const getValue = (patientValue) => {
+  if (Array.isArray(patientValue)) {
+    return patientValue.length;
+  }
+  return patientValue === true ? 1 : 0;
+}
+
+const allValuesEqual = (valueArray) => {
+  const compareValue = valueArray[0].value;
+  for (let k = 1; k < valueArray.length; k += 1) {
+    if (compareValue !== valueArray[k].value) {
+      return false;
+    }
+  }
+  return true;
+}
+
 const formatData = (patientResults, selectedMeasures, storeInfo) => {
   const formattedData = [];
-  const numeratorValues = []
-  const denominatorsValues = []
 
-  for (let i = 0; i < patientResults.length; i += 1) {
-    const patientDetails = patientResults[i][patientResults[i].memberId]
+  patientResults.forEach((patientResult) => {
+    const patientDetails = patientResult[patientResult.memberId]
     const detailKeys = Object.keys(patientDetails)
     const denominatorsFound = []
     const numeratorsFound = []
@@ -41,42 +65,45 @@ const formatData = (patientResults, selectedMeasures, storeInfo) => {
       } else if (key.includes('Denominator')) {
         denominatorsFound.push(key)
       }
-    })
+    });
+    const patientResultArray = []
     numeratorsFound.forEach((numerator, index) => {
-      const numValue = patientDetails[numerator]
-      const numeratorObject = {
-        memberID: patientResults[i].memberId,
-        measure: selectedMeasures[index + 1],
-        label: storeInfo[selectedMeasures[index + 1]].displayLabel,
-        [numerator]: numValue,
+      const numValue = getValue(patientDetails[numerator]);
+      let denValue = 0;
+      let resultObject = {};
+      let useIndex = 0;
+      if (numeratorsFound.length === 1) {
+        denValue = getValue(patientDetails.Denominator);
+      } else {
+        useIndex = index + 1;
+        denValue = getValue(patientDetails[`Denominator ${useIndex}`]);
       }
-      numeratorValues.push(numeratorObject)
-    })
-    denominatorsFound.forEach((denominators, index) => {
-      const denValue = patientDetails[denominators]
-      const denominatorsObject = {
-        memberID: patientResults[i].memberId,
-        measure: selectedMeasures[index + 1],
-        label: storeInfo[selectedMeasures[index + 1]].displayLabel,
-        [denominators]: denValue,
+      resultObject = {
+        memberID: patientResult.memberId,
+        measure: selectedMeasures[useIndex],
+        label: storeInfo[selectedMeasures[useIndex]].displayLabel,
+        value: numValue === denValue,
       }
-      denominatorsValues.push(denominatorsObject)
-    })
-
-    formattedData.push({
-      value: patientResults[i].memberId,
-      label: patientResults[i].memberId,
-      type: 'patient',
-      included: 1,
-      numeratorValues: 1,
-      numerator: 1,
-      denominator: 1,
-      exclusions: 1,
-
+      patientResultArray.push(resultObject);
     });
 
-    
-  }
+    const formattedResult = {
+      value: patientResult.memberId,
+      label: patientResult.memberId,
+      type: 'patient',
+    }
+
+    if (patientResultArray.length === 1) {
+      formattedResult[selectedMeasures[0]] = patientResultArray[0].value.toString()
+    } else {
+      formattedResult[selectedMeasures[0]] = allValuesEqual(patientResultArray).toString();
+      for (let k = 1; k < selectedMeasures.length; k += 1) {
+        formattedResult[selectedMeasures[k]] = patientResultArray[k - 1].value.toString();
+      }
+    }
+
+    formattedData.push(formattedResult);
+  });
 
   return formattedData;
 };
