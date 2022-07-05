97179938f3c6fef90223a7d7abb846c9ab14b538
Maud <maudlin.oxalis@gmail.com>
2022-07-05 16:41:29 -0400

    lint fix

--- src/components/DisplayTable/MeasureTableRow.js
+++ src/components/DisplayTable/MeasureTableRow.js
@@ -41,31 +41,29 @@ function MeasureTableRow({
             color={color}
           />
         )}
-        {headerInfo.map((fieldInfo) => {
-          return (
-            <Grid
-              item
-              className={`measure-table-row__data-align measure-table-row__data-align--${fieldInfo.flexBasis}`}
-              key={`${rowDataItem[fieldInfo.key]}-${fieldInfo.header}`}
+        {headerInfo.map((fieldInfo) => (
+          <Grid
+            item
+            className={`measure-table-row__data-align measure-table-row__data-align--${fieldInfo.flexBasis}`}
+            key={`${rowDataItem[fieldInfo.key]}-${fieldInfo.header}`}
+          >
+            <Typography
+              variant="caption"
+              className="measure-table-row__data"
             >
-              <Typography
-                variant="caption"
-                className="measure-table-row__data"
-              >
-  
-                {fieldInfo.link && rowDataItem.value !== 'composite'
-                  ? (
-                    <Tooltip title={measureInfo[rowDataItem.value].title} arrow>
-                      <Link to={{ pathname: `/${rowDataItem.value}` }}>
-                        {rowDataItem[fieldInfo.key]}
-                      </Link>
-                    </Tooltip>
-                  )
-                  : subMeasureCheck(fieldInfo, rowDataItem)}
-              </Typography>
-            </Grid>
-          )
-        })}
+
+              {fieldInfo.link && rowDataItem.value !== 'composite'
+                ? (
+                  <Tooltip title={measureInfo[rowDataItem.value].title} arrow>
+                    <Link to={{ pathname: `/${rowDataItem.value}` }}>
+                      {rowDataItem[fieldInfo.key]}
+                    </Link>
+                  </Tooltip>
+                )
+                : subMeasureCheck(fieldInfo, rowDataItem)}
+            </Typography>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   )
