43f47ee2f8c7c46aeee9b64e203ef998061215c2
Maud <maudlin.oxalis@gmail.com>
2022-07-08 14:02:56 -0400

    added tertiary them for components, updated filter color

--- src/components/ChartContainer/ChartBar.js
+++ src/components/ChartContainer/ChartBar.js
@@ -87,7 +87,7 @@ function ChartBar({
         <Grid item sx={buttonStyling}>
           <Button
             key="d3-YTD"
-            color="primary"
+            color="tertiary"
             onClick={handleDateOpen}
             variant="text"
             startIcon={<DateRangeIcon />}
@@ -141,7 +141,7 @@ function ChartBar({
                   </LocalizationProvider>
                   <Grid container justifyContent="center" sx={{ m: '10px', ml: '-10px' }}>
                     <Grid item>
-                      <Button variant="contained" color="primary" onClick={clearDate}>
+                      <Button variant="contained" color="tertiary" onClick={clearDate}>
                         Clear Selection
                       </Button>
                     </Grid>
@@ -179,7 +179,7 @@ function ChartBar({
           <Badge badgeContent={filterSum} className={`chart-bar__badge${filterDisabled ? '--hidden' : ''}`}>
             <Button
               className="chart-bar__filter-button"
-              color="primary"
+              color="tertiary"
               variant="text"
               onClick={onClickFilter}
               disabled={filterDisabled}
