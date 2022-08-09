2985131cbc1b9731933936cdce916f27b078efbc
Maud <maudlin.oxalis@gmail.com>
2022-08-09 10:04:12 -0400

    removed code smells

--- src/test/components/ChartContainer/Login.test.js
+++ src/test/components/ChartContainer/Login.test.js
@@ -11,13 +11,11 @@ describe('Login', () => {
 describe('Links render as expected', () => {
   it('2 Links render on page', () => {
     render(<Login />)
-    // links
     const linksOnPage = document.getElementsByTagName('a')
     expect(linksOnPage.length).toBe(2)
   });
   it('"Sign Up" link renders to page with correct text and href', () => {
     render(<Login />)
-    // links
     const linksOnPage = document.getElementsByTagName('a')
     const signUpLink = linksOnPage[0]
     expect(signUpLink.href.includes('/auth/register')).toBeTruthy()
@@ -25,7 +23,6 @@ describe('Links render as expected', () => {
   });
   it('"Forgot Password" link renders to page with correct text and href', () => {
     render(<Login />)
-    // links
     const linksOnPage = document.getElementsByTagName('a')
     const forgotPasswordLink = linksOnPage[1]
     expect(forgotPasswordLink.href.includes('#pablo')).toBeTruthy()
@@ -36,20 +33,17 @@ describe('Links render as expected', () => {
 describe('Labels render as expected', () => {
   it('2 Labels render on page', () => {
     render(<Login />)
-    // labels
     const labelsOnPage = document.getElementsByTagName('label')
     expect(labelsOnPage.length).toBe(2)
   });
   it('"Username/Email" label renders to page with correct placeholder', () => {
     render(<Login />)
-    // // labels
     const labelsOnPage = document.getElementsByTagName('label')
     const usernameEmailLabel = labelsOnPage[0]
     expect(usernameEmailLabel.innerHTML.includes('Username/Email')).toBe(true)
   });
   it('"Password" label renders to page with correct placeholder', () => {
     render(<Login />)
-    // // labels
     const labelsOnPage = document.getElementsByTagName('label')
     const passwordLabel = labelsOnPage[1]
 
@@ -60,13 +54,11 @@ describe('Labels render as expected', () => {
 describe('Inputs render as expected', () => {
   it('2 Inputs render on page', () => {
     render(<Login />)
-    // inputs
     const inputsOnPage = document.getElementsByTagName('input')
     expect(inputsOnPage.length).toBe(2)
   });
   it('"Username/Email" input renders to page with correct text', () => {
     render(<Login />)
-    // inputs
     const inputsOnPage = document.getElementsByTagName('input')
 
     const usernameEmailLabel = inputsOnPage[0]
@@ -74,7 +66,6 @@ describe('Inputs render as expected', () => {
   });
   it('"Password" input renders to page with correct text', () => {
     render(<Login />)
-    // inputs
     const inputsOnPage = document.getElementsByTagName('input')
     const passwordLabel = inputsOnPage[1]
 
@@ -91,8 +82,6 @@ describe('Button renders as expected', () => {
 
   it('Google button renders with correct text', () => {
     render(<Login />)
-    // buttons
-    // const googleButton = screen.getByText('Sign in with Google')
     const googleButton = screen.getByRole('button', { name: 'google.svg Sign in with Google' })
     expect(googleButton.textContent.includes('Sign in with Google')).toBe(true)
   })
@@ -102,8 +91,6 @@ describe('Button renders as expected', () => {
     const loginButton = screen.getByRole('button', { name: 'Login' })
     expect(loginButton.textContent.includes('Login')).toBe(true)
   })
-
-  // it: login button passes username and password to be processed and we move to the dashboard
 })
 
 describe('Sign up link renders and navigates as expected', () => {
@@ -111,7 +98,6 @@ describe('Sign up link renders and navigates as expected', () => {
     render(<Login />)
     const registerLink = screen.getByRole('link', { name: 'Sign Up' })
     expect(registerLink.hasAttribute('href', '/auth/register')).toBe(true)
-    // it: check if history changes to dashboard
   })
 })
 
@@ -123,17 +109,3 @@ describe('Google OAuth logging in', async () => {
     expect(fireEvent.click(googleLogin)).toBe(true)
   })
 })
-``
-// // expect(signUpLink.href).to.equal('/auth/register')
-// // console.log(signUpLink.length)
-// screen.debug()
-
-// label for Username/Email
-// input for Username/Email
-// label for Password
-// input for Password
-// 2 butttons
-
-// gooogle OAUTH WORKS
-// login button WORKS
-// both inputs work (receive text)
