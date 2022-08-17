c79e3b4efcfad65ec883190fa5326dfd9909aa7d
Maud <maudlin.oxalis@gmail.com>
2022-08-17 10:01:23 -0400

    moved over previous written tests and added new info

--- src/test/components/MemberReport/MemberReportDisplay.test.js
+++ src/test/components/MemberReport/MemberReportDisplay.test.js
@@ -1,15 +1,74 @@
 import {
-  fireEvent, render, screen,
+  waitForElementToBeRemoved, getByText, userEvent, render, screen, waitFor, cleanup,
 } from '@testing-library/react';
-import MemberReport from 'layouts/MemberReport';
-import MemberReportDisplay from '../../../components/MemberReport/MemberReportDisplay'
 
-describe('Member Report Display', () => {
-  it('Renders on Load', () => {
-    render(
-      <MemberReport>
-        <MemberReportDisplay />
-      </MemberReport>
+import { DatastoreContext } from 'context/DatastoreProvider';
+import MemberReport from '../../../layouts/MemberReport'
+import MemberReportDisplay from '../../../components/MemberReport/MemberReportDisplay'
+import { DatastoreReducer } from '../../../context/DatastoreReducer';
+import { resultList, infoObject, memberId, memberInfo } from '../../data/DemoData';
+
+describe('Member view page', () => {
+  const mockInitState = {
+    results: [],
+    trends: [],
+    currentResults: [],
+    info: {},
+    lastUpdated: 'Updating now...',
+  }
+
+  const datastore = DatastoreReducer(mockInitState, { type: 'SET_RESULTS', payload: { results: resultList, info: infoObject } })
+  // const mockToggleData = jest.fn(() => false)
+
+  // providing data context to components that rely on the data store in our future tests is essential
+  beforeEach(async () => {
+    const container = render(
+      <DatastoreContext.Provider value={{ datastore }}>
+        <MemberReport
+          id={memberId}
+        >
+          <MemberReportDisplay
+            id={memberId}
+            memberInfo={memberInfo}
+            datastoreInfo={datastore.info}
+            exportUrl={exportUrl}
+            coverageStatus={coverageStatus}
+            rowData={rowData}
+            description={description}
+          />
+        </MemberReport>
+      </DatastoreContext.Provider>,
     )
-  });
-});
+    await waitFor(() => container.getByRole('heading', { name: "Reporting - Member's Data" }))
+    await waitForElementToBeRemoved(() => container.getByText('Fetching...'))
+    // jest.setTimeout(10000)
+  })
+
+  afterEach(async () => {
+    cleanup
+  })
+
+  it('Headings rendered', () => {
+    expect(screen.getAllByRole('heading').length).toBe(4)
+    expect(screen.getByRole('heading', { name: "Reporting - Member's Data" })).not.toBeNull()
+    expect(screen.getByRole('heading', { name: 'General Information' })).not.toBeNull()
+    expect(screen.getByRole('heading', { name: 'Measure Analysis' })).not.toBeNull()
+    expect(screen.getByRole('heading', { name: 'AAB - Avoidance of Antibiotic Treatment in Adults with Acute Bronchitis' })).not.toBeNull()
+  })
+
+  it('Buttons are loaded', () => {
+    expect(screen.getAllByRole('button').length).toBe(4)
+    expect(screen.getByRole('button', { name: 'Export' })).not.toBeNull()
+    expect(screen.getByRole('button', { name: 'AAB - Avoidance of Antibiotic Treatment in Adults with Acute Bronchitis' })).not.toBeNull()
+  })
+
+  // export button exists, is clickable
+  // general info pop up has information
+  // general information has all the necessary information
+  // member id, dob, age, gender, coverage status, participation period
+  // policy id, payor, plan, dependents, relationship, type, participation period
+  // measure analysis pop up has information
+  // measure analysis drop down renders
+  // measure analysis has all the necessary information
+  // measure, type, status, exclusions, practitioner, dates, conditions, and recommendations render
+})
