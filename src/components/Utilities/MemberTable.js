e65f70aa7d1f1c9dd7f88c73df4686b368562576
Tim <tim@Amidas-MacBook-Pro.local>
2022-09-19 14:33:37 -0400

    wip

--- src/components/Utilities/MemberTable.js
+++ src/components/Utilities/MemberTable.js
@@ -47,55 +47,46 @@ const formatData = (memberResults, activeMeasure, storeInfo, tableFilter) => {
 
   const subMeasures = Object.keys(storeInfo).filter((item) => item.includes(activeMeasure));
 
-  memberResults.forEach((memberResult) => {
-    const memberResultArray = [];
-    const complianceResult = getMeasureCompliance(memberResult);
-    if (complianceResult.length === 1) {
-      memberResultArray.push({
-        memberID: memberResult.memberId,
-        measure: subMeasures[0],
-        label: storeInfo[subMeasures[0]].displayLabel,
-        value: complianceResult[0],
-      });
-    } else {
-      complianceResult.forEach((result, index) => {
+  if (memberResults) {
+    memberResults.forEach((memberResult) => {
+      const memberResultArray = [];
+      const complianceResult = getMeasureCompliance(memberResult);
+      if (complianceResult.length === 1) {
         memberResultArray.push({
           memberID: memberResult.memberId,
-          measure: subMeasures[index + 1],
-          label: storeInfo[subMeasures[index + 1]]?.displayLabel,
-          value: result,
+          measure: subMeasures[0],
+          label: storeInfo[subMeasures[0]].displayLabel,
+          value: complianceResult[0],
         });
-      });
-    }
-
-    const formattedResult = {
-      value: memberResult.memberId,
-      label: memberResult.memberId,
-      type: 'member',
-    }
-
-    if (memberResultArray.length === 1) {
-      formattedResult[subMeasures[0]] = memberResultArray[0].value.toString()
-    } else {
-      // console.log({ memberResultArray })
-      // console.log(allValuesEqual(memberResultArray))
-      // console.log(allValuesEqual(memberResultArray).toString())
-
-      formattedResult[subMeasures[0]] = allValuesEqual(memberResultArray).toString();
-      // console.log(formattedResult[subMeasures[0]])
-      // console.log(subMeasures.length)
-      // console.log(subMeasures)
-      for (let k = 1; k < subMeasures.length; k += 1) {
-        // console.log("IN FoR LOOP", memberResultArray[k - 1])
-        // if (formattedResult[subMeasures[k]]) {
-        formattedResult[subMeasures[k]] = memberResultArray[k - 1].value.toString();
-        // }
+      } else {
+        complianceResult.forEach((result, index) => {
+          memberResultArray.push({
+            memberID: memberResult.memberId,
+            measure: subMeasures[index + 1],
+            label: storeInfo[subMeasures[index + 1]]?.displayLabel,
+            value: result,
+          });
+        });
       }
-    }
 
-    formattedData.push(formattedResult);
-  });
+      const formattedResult = {
+        value: memberResult.memberId,
+        label: memberResult.memberId,
+        type: 'member',
+      }
 
+      if (memberResultArray.length === 1) {
+        formattedResult[subMeasures[0]] = memberResultArray[0].value.toString()
+      } else {
+        formattedResult[subMeasures[0]] = allValuesEqual(memberResultArray).toString();
+        for (let k = 1; k < subMeasures.length; k += 1) {
+          formattedResult[subMeasures[k]] = memberResultArray[k - 1].value.toString();
+        }
+      }
+
+      formattedData.push(formattedResult);
+    });
+  }
   return filterByNonCompliance(formattedData, tableFilter);
 };
 
