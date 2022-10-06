ad5dedc07b250e67e220a61baa8ea72b12bb63ba
Maud <maudlin.oxalis@gmail.com>
2022-10-06 14:54:09 -0600

    no scss time

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -14,10 +14,10 @@ const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
 export default function Navbar() {
   return (
-    <Box className="navbar" sx={{ flexGrow: 1 }}>
-      <AppBar className="navbar__container">
-        <Toolbar className="navbar__toolbar">
-          <div className="navbar__link-holder-left">
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ backgroundColor: (theme) => theme.palette.bluegray?.D4 }}>
+        <Toolbar position="fixed">
+          <Box>
             <Link
               sx={{ color: (theme) => theme.palette.bluegray?.L5 }}
               component={RouterLink}
@@ -26,9 +26,8 @@ export default function Navbar() {
             >
               Saraswati
             </Link>
-          </div>
-
-          <div className="navbar__link-holder-right">
+          </Box>
+          <Box sx={{ flexGrow: 1, textAlign: 'end' }}>
             <Link
               sx={{ color: (theme) => theme.palette.bluegray?.L5 }}
               component={RouterLink}
@@ -54,10 +53,60 @@ export default function Navbar() {
             >
               Sign Out
             </Link>
-          </div>
+          </Box>
         </Toolbar>
       </AppBar>
       <Offset />
     </Box>
-  );
+  )
 }
+
+// export default function Navbar() {
+//   return (
+//     <Box className="navbar" sx={{ flexGrow: 1 }}>
+//       <AppBar className="navbar__container">
+//         <Toolbar className="navbar__toolbar">
+//           <Box className="navbar__link-holder-left">
+//             <Link
+//               sx={{ color: (theme) => theme.palette.bluegray?.L5 }}
+//               component={RouterLink}
+//               className="navbar__text-nav-dashboard"
+//               to="/"
+//             >
+//               Saraswati
+//             </Link>
+//           </Box>
+
+//           <Box className="navbar__link-holder-right">
+//             <Link
+//               sx={{ color: (theme) => theme.palette.bluegray?.L5 }}
+//               component={RouterLink}
+//               className="navbar__text"
+//               to={{ pathname: '/' }}
+//             >
+//               Dashboard
+//             </Link>
+//             <Link
+//               sx={{ color: (theme) => theme.palette.bluegray?.L5 }}
+//               component={RouterLink}
+//               className="navbar__text"
+//               to={{ pathname: '/reports/' }}
+//             >
+//               Reports
+//             </Link>
+//             <Link
+//               sx={{ color: (theme) => theme.palette.bluegray?.L5 }}
+//               component={RouterLink}
+//               className="navbar__text-signout"
+//               to={{ pathname: '/auth/login' }}
+//               onClick={logout}
+//             >
+//               Sign Out
+//             </Link>
+//           </Box>
+//         </Toolbar>
+//       </AppBar>
+//       <Offset />
+//     </Box>
+//   );
+// }
