2000e763a8d1d7f80acf5ac12eb8f43b90e37aaa
Tim <tim@Amidas-MacBook-Pro.local>
2022-10-26 14:42:09 -0400

    wip

--- src/test/components/FilterMenu/FilterDrawer.test.js
+++ src/test/components/FilterMenu/FilterDrawer.test.js
@@ -2,12 +2,17 @@ import {
   fireEvent, render, screen,
 } from '@testing-library/react';
 import FilterDrawer from '../../../components/FilterMenu/FilterDrawer';
+import { additionalFilterOptions } from '../../data/DemoData';
 
 const filters = {
-  domainsOfCare: ['EOC'],
-  stars: [1],
-  percentRange: [5, 95],
-  sum: 3,
+  domainsOfCare: [],
+  stars: [],
+  percentRange: [0, 100],
+  sum: 0,
+  healthcareCoverages: [],
+  healthcarePractitioners: [],
+  healthcareProviders: [],
+  payors: [],
 };
 
 const mockHandleFilterChange = jest.fn(() => false);
@@ -22,7 +27,7 @@ const givenPropsTest = async (getByText) => {
   expect(screen.getByDisplayValue('2').checked).toBe(false);
 }
 
-const closeOpenDrawerTest = async (rerender) => {
+const closeOpenDrawerTest = (rerender) => {
   rerender(<FilterDrawer
     filterDrawerOpen={false}
   />);
@@ -39,97 +44,122 @@ describe('FilterDrawer', () => {
     const { getByText, rerender } = render(
       <FilterDrawer
         filterDrawerOpen
+        currentFilters={filters}
+        additionalFilterOptions={additionalFilterOptions}
         handleFilterChange={mockHandleFilterChange}
         toggleFilterDrawer={mockToggleFilterDrawer}
       />,
     );
-
-    fireEvent.click(screen.getByDisplayValue('ECDS'));
-    fireEvent.click(screen.getByDisplayValue('2'));
-    fireEvent.click(screen.getByDisplayValue('3'));
-
-    expect(screen.getByDisplayValue('EOC').checked).toBe(false);
-    expect(screen.getByDisplayValue('ECDS').checked).toBe(true);
-    expect(screen.getByDisplayValue('1').checked).toBe(false);
-    expect(screen.getByDisplayValue('2').checked).toBe(true);
-    expect(screen.getByDisplayValue('3').checked).toBe(true);
-    expect(screen.getByDisplayValue('4').checked).toBe(false);
-    expect(screen.getByDisplayValue('5').checked).toBe(false);
-
-    fireEvent.click(getByText('Apply Filters'));
-    expect(mockHandleFilterChange).toHaveBeenCalledWith({
-      domainsOfCare: ['ECDS'],
-      stars: [2, 3],
-      percentRange: [0, 100],
-      sum: 3,
+    const checkboxes = screen.getAllByRole('checkbox');
+    const checkboxesReal = [...checkboxes]
+    const expectedValues = {
+      EOS: false,
+      ECDS: true,
+      1: false,
+      2: true,
+      3: true,
+      4: false,
+      5: false,
+    };
+    // console.log("--------->", key, checkboxesReal.find((box) => box.value === key))
+    // console.log(checkboxes)
+    // console.log(checkboxesReal.map((chek)=>chek.value))
+    Object.entries(expectedValues).forEach(([key, value]) => {
+      if (value) {
+        fireEvent.click(checkboxesReal.find((box) => box.value === key))
+      }
+      expect(checkboxesReal.find((box) => box.checked))
     });
-    expect(mockToggleFilterDrawer).toHaveBeenCalledWith(false);
 
-    rerender(<FilterDrawer
-      filterDrawerOpen={false}
-    />);
-    const refineByText = screen.queryByText('Refine by');
-    expect(refineByText).toBe(null);
+    // expect(screen.getByDisplayValue('EOC').checked).toBe(false);
+    // expectedValues.forEach((value) => fireEvent.click(screen.getByDisplayValue(value)))
+    // expect(screen.getByDisplayValue('ECDS').checked).toBe(true);
+    // expect(screen.getByDisplayValue('1').checked).toBe(false);
+    // expect(screen.getByDisplayValue('2').checked).toBe(true);
+    // expect(screen.getByDisplayValue('3').checked).toBe(true);
+    // expect(screen.getByDisplayValue('4').checked).toBe(false);
+    // expect(screen.getByDisplayValue('5').checked).toBe(false);
 
-    rerender(<FilterDrawer
-      filterDrawerOpen
-    />);
-    expect(screen.getByDisplayValue('ECDS').checked).toBe(true);
-    expect(screen.getByDisplayValue('2').checked).toBe(true);
-    expect(screen.getByDisplayValue('3').checked).toBe(true);
+    // fireEvent.click(getByText('Apply Filters'));
+    // expect(mockHandleFilterChange).toHaveBeenCalledWith({
+    //   domainsOfCare: ['ECDS'],
+    //   stars: [2, 3],
+    //   percentRange: [0, 100],
+    //   sum: 3,
+    // });
+    // expect(mockToggleFilterDrawer).toHaveBeenCalledWith(false);
+
+    // rerender(<FilterDrawer
+    //   filterDrawerOpen={false}
+    // />);
+    // const refineByText = screen.queryByText('Refine by');
+    // expect(refineByText).toBe(null);
+
+    // rerender(<FilterDrawer
+    //   filterDrawerOpen
+    // />);
+    // expect(screen.getByDisplayValue('ECDS').checked).toBe(true);
+    // expect(screen.getByDisplayValue('2').checked).toBe(true);
+    // expect(screen.getByDisplayValue('3').checked).toBe(true);
   })
 
-  test('resets to the default filter state', () => {
-    const { getByText, rerender } = render(
-      <FilterDrawer
-        filterDrawerOpen
-        handleFilterChange={mockHandleFilterChange}
-        currentFilters={filters}
-        toggleFilterDrawer={mockToggleFilterDrawer}
-      />,
-    );
+  // test('resets to the default filter state', () => {
+  //   const { getByText, rerender } = render(
+  //     <FilterDrawer
+  //       filterDrawerOpen
+  //       handleFilterChange={mockHandleFilterChange}
+  //       currentFilters={filters}
+  //       additionalFilterOptions={additionalFilterOptions}
+  //       toggleFilterDrawer={mockToggleFilterDrawer}
+  //     />,
+  //   );
 
-    givenPropsTest(getByText);
-    fireEvent.click(getByText('Reset Filters'));
-    expect(mockHandleFilterChange).toHaveBeenCalledWith({
-      domainsOfCare: [],
-      stars: [],
-      percentRange: [0, 100],
-      sum: 0,
-    });
-    expect(mockToggleFilterDrawer).toHaveBeenCalledWith(false);
+  //   givenPropsTest(getByText);
+  //   fireEvent.click(getByText('Reset Filters'));
+  //   expect(mockHandleFilterChange).toHaveBeenCalledWith({
+  //     domainsOfCare: [],
+  //     stars: [],
+  //     percentRange: [0, 100],
+  //     sum: 0,
+  //     healthcareCoverages: [],
+  //     healthcarePractitioners: [],
+  //     healthcareProviders: [],
+  //     payors: [],
+  //   });
+  //   expect(mockToggleFilterDrawer).toHaveBeenCalledWith(false);
 
-    closeOpenDrawerTest(rerender);
-    expect(screen.getByDisplayValue('EOC').checked).toBe(false);
-    expect(screen.getByDisplayValue('ECDS').checked).toBe(false);
-    expect(screen.getByDisplayValue('1').checked).toBe(false);
-    expect(screen.getByDisplayValue('2').checked).toBe(false);
-  });
+  //   closeOpenDrawerTest(rerender);
+  //   expect(screen.getByDisplayValue('EOC').checked).toBe(false);
+  //   expect(screen.getByDisplayValue('ECDS').checked).toBe(false);
+  //   expect(screen.getByDisplayValue('1').checked).toBe(false);
+  //   expect(screen.getByDisplayValue('2').checked).toBe(false);
+  // });
 
-  test('cancels the filter changes', () => {
-    const { getByText, rerender } = render(
-      <FilterDrawer
-        filterDrawerOpen
-        handleFilterChange={mockHandleFilterChange}
-        currentFilters={filters}
-        toggleFilterDrawer={mockToggleFilterDrawer}
-      />,
-    );
+  // test('cancels the filter changes', () => {
+  //   const { getByText, rerender } = render(
+  //     <FilterDrawer
+  //       filterDrawerOpen
+  //       handleFilterChange={mockHandleFilterChange}
+  //       currentFilters={filters}
+  //       additionalFilterOptions={additionalFilterOptions}
+  //       toggleFilterDrawer={mockToggleFilterDrawer}
+  //     />,
+  //   );
 
-    givenPropsTest(getByText);
-    expect(screen.getByDisplayValue('ECDS').checked).toBe(false);
-    expect(screen.getByDisplayValue('2').checked).toBe(false);
-    fireEvent.click(screen.getByDisplayValue('ECDS'));
-    fireEvent.click(screen.getByDisplayValue('2'));
-    expect(screen.getByDisplayValue('ECDS').checked).toBe(true);
-    expect(screen.getByDisplayValue('2').checked).toBe(true);
+  //   givenPropsTest(getByText);
+  //   expect(screen.getByDisplayValue('ECDS').checked).toBe(false);
+  //   expect(screen.getByDisplayValue('2').checked).toBe(false);
+  //   fireEvent.click(screen.getByDisplayValue('ECDS'));
+  //   fireEvent.click(screen.getByDisplayValue('2'));
+  //   expect(screen.getByDisplayValue('ECDS').checked).toBe(true);
+  //   expect(screen.getByDisplayValue('2').checked).toBe(true);
 
-    fireEvent.click(getByText('Cancel'));
-    expect(mockToggleFilterDrawer).toHaveBeenCalledWith(false);
-    expect(mockHandleFilterChange).not.toHaveBeenCalled();
+  //   fireEvent.click(getByText('Cancel'));
+  //   expect(mockToggleFilterDrawer).toHaveBeenCalledWith(false);
+  //   expect(mockHandleFilterChange).not.toHaveBeenCalled();
 
-    closeOpenDrawerTest(rerender);
-    expect(screen.getByDisplayValue('ECDS').checked).toBe(false);
-    expect(screen.getByDisplayValue('2').checked).toBe(false);
-  });
+  //   closeOpenDrawerTest(rerender);
+  //   expect(screen.getByDisplayValue('ECDS').checked).toBe(false);
+  //   expect(screen.getByDisplayValue('2').checked).toBe(false);
+  // });
 })
