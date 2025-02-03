dc6bfa15adbee5ed38fa32f247923604b5258791
jfetterolf <jonathan.fetterolf@amida.com>
2025-02-03 17:18:52 -0500

    standard button branch coverage & remove dup code

--- src/test/common/CommonStandardButton.test.js
+++ src/test/common/CommonStandardButton.test.js
@@ -1,36 +1,69 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, act } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { StandardButton } from 'components/Common/CommonStandardButton';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 describe('StandardButton', () => {
-  it('contained variant renders correctly', () => {
-    render(<StandardButton variant='contained' />);
+  it('renders the contained button with left and right icons correctly', () => {
+    // Render a contained variant StandardButton
+    render(
+      <StandardButton
+        variant='contained'
+        leftIcon={<CheckCircleIcon />}
+        rightIcon={<CheckBoxIcon />}
+      >
+        Wrapped Text
+      </StandardButton>,
+    );
+    // Identify the button and assert it exists
     const btn = screen.getByRole('button');
+    expect(btn).toBeInTheDocument();
+    // Assert the button has the correct classes
     expect(btn.className).toContain(
       'MuiButton-contained MuiButton-containedPrimary',
     );
-    expect(btn).toBeInTheDocument();
+    // Clicks the standard button w/ default onClick prop
+    act(() => {
+      fireEvent.click(btn);
+    });
+    // Identify the left and right icons and assert they exist
+    const leftIcon = screen.getByTestId('CheckCircleIcon');
+    const rightIcon = screen.getByTestId('CheckBoxIcon');
+    expect(leftIcon).toBeInTheDocument();
+    expect(rightIcon).toBeInTheDocument();
+    // Identify the wrapped text and assert it exists
+    const wrappedText = screen.getByText(/wrapped text/i);
+    expect(wrappedText).toBeInTheDocument();
   });
 
   it('outlined variant renders correctly', () => {
+    // Render a outlined variant StandardButton
     render(<StandardButton variant='outlined' />);
+    // Identify the button and assert it exists
     const btn = screen.getByRole('button');
+    expect(btn).toBeInTheDocument();
+    // Assert the button has the correct classes
     expect(btn.className).toContain(
       'MuiButton-outlined MuiButton-outlinedPrimary',
     );
-    expect(btn).toBeInTheDocument();
   });
 
   it('text variant renders correctly', () => {
+    // Render a text variant StandardButton
     render(<StandardButton variant='text' />);
+    // Identify the button and assert it exists
     const btn = screen.getByRole('button');
-    expect(btn.className).toContain('MuiButton-text MuiButton-textPrimary');
     expect(btn).toBeInTheDocument();
+    // Assert the button has the correct classes
+    expect(btn.className).toContain('MuiButton-text MuiButton-textPrimary');
   });
 
   it('an undefined variant renders correctly', () => {
+    // Render an undefined variant StandardButton
     const variant = 'best-btn';
     render(<StandardButton variant={variant} />);
+    // Identify the button and assert it exists
     const btn = screen.getByRole('button');
     expect(btn.className).toContain(
       `MuiButton-${variant} MuiButton-${variant}Primary`,
