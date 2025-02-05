3c8bacd100e371893866b6c169d10b9be0420a02
jfetterolf <jonathan.fetterolf@amida.com>
2025-02-05 15:11:00 -0500

    periodic - SavedQueries tests

---
+++ src/test/components/Reports/SavedQueries.test.js
@@ -0,0 +1,16 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import SavedQueries from 'components/Reports/SavedQueries';
+
+describe('SavedQueries.js', () => {
+    it('renders without crashing', () => {
+        // Render the saved queries
+        render(<SavedQueries />);
+        // Identify all list items
+        const listItems = screen.getAllByRole('listitem');
+        // Identify all links
+        const links = screen.getAllByRole('link');
+        // Assert 4 listItems and for links
+        expect(listItems && links).toHaveLength(4);
+    });
+});
