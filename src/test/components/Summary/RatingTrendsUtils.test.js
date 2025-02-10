bad6ff27a7c70d1552255fa25af9238d775e7641
jfetterolf <jonathan.fetterolf@amida.com>
2025-02-10 15:12:29 -0500

    qube comments - dup code

--- src/test/components/Summary/RatingTrendsUtils.test.js
+++ src/test/components/Summary/RatingTrendsUtils.test.js
@@ -1,19 +1,18 @@
 import '@testing-library/jest-dom';
-import { sortedTrendsCreator } from 'components/Summary/RatingTrendsUtils';
-import {
-    trendsImae,
-    activeMeasureImae,
-    measureTrendImae,
-    sortedTrendsResultImae,
-    activeMeasureComposite,
-    measureTrendComposite,
-    sortedTrendsResultComposite,
-    trendsComposite,
-    infoMain,
-    activeMeasureMain,
-    measureTrendMain, resultMain
-} from 'test/resources/constants/RatingTrendsUtilsConstants';
 import { mainTrendCreator } from 'components/Summary/RatingTrendsUtils';
+import { sortedTrendsCreator } from 'components/Summary/RatingTrendsUtils';
+import activeMeasureComposite from './resources/json/activeMeasureComposite.json';
+import activeMeasureImae from './resources/json/activeMeasureImae.json';
+import activeMeasureMain from './resources/json/activeMeasureMain.json';
+import infoMain from './resources/json/infoMain.json';
+import measureTrendComposite from './resources/json/measureTrendComposite.json';
+import measureTrendImae from './resources/json/measureTrendImae.json';
+import measureTrendMain from './resources/json/measureTrendMain.json';
+import resultMain from './resources/json/resultMain.json';
+import sortedTrendsResultComposite from './resources/json/sortedTrendsResultComposite.json';
+import sortedTrendsResultImae from './resources/json/sortedTrendsResultImae.json';
+import trendsComposite from './resources/json/trendsComposite.json';
+import trendsImae from './resources/json/trendsImae.json';
 
 // sortedTrendsCreator(activeMeasure, trends, measureTrend)
 describe('sortedTrendsCreator(activeMeasure, trends, measureTrend)', () => {
